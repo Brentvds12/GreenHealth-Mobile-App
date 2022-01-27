0a661e3e48f4e5b8bdad929b37a91774cbc7b713
Brentvds12 <r0793295@student.thomasmore.be>
2022-01-27 10:04:18 +0100

    initial commit

---
+++ GreenHealth_Mobile_App/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("ProximaNova-Black.otf", Alias = "PN-Black")]
+[assembly: ExportFont("ProximaNova-Boldlt.otf", Alias = "PN-Boldlt")]
+[assembly: ExportFont("ProximaNova-Extrabld.otf", Alias = "PN-Extrabld")]
+[assembly: ExportFont("ProximaNova-Light.otf", Alias = "PN-Light")]
+[assembly: ExportFont("ProximaNova-Lightlt.otf", Alias = "PN-Lightlt")]
+[assembly: ExportFont("ProximaNova-Regular.otf", Alias = "PN-Regular")]
+[assembly: ExportFont("ProximaNova-Regularlt.otf", Alias = "PN-Regularlt")]
+[assembly: ExportFont("ProximaNova-Semibold.otf", Alias = "PN-Semibold")]
+[assembly: ExportFont("ProximaNova-Semiboldlt.otf", Alias = "PN-Semiboldlt")]
+[assembly: ExportFont("vicons.woff", Alias = "Vicons")]
---
+++ GreenHealth_Mobile_App/CameraPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenHealth_Mobile_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+        }
+
+        async void PickButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please pick a photo"
+            });
+
+            var stream = await result.OpenReadAsync();
+
+            resultImage.Source = ImageSource.FromStream(() => stream);
+        }
+
+        async void MakeButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync();
+
+            var stream = await result.OpenReadAsync();
+
+            resultImage.Source = ImageSource.FromStream(() => stream);
+        }
+    }
+}
